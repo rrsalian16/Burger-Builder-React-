f357ea9949adc77af5f997063f6eb79d3cd2d294
rrsalian16 <rrsalian16@gmail.com>
2020-06-04 16:22:28 +0530

    Routing is completed.

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Order.module.css';
+import classes from "./Order.module.css";
 
-const order=(props)=>(
+const order = (props) => {
+  const ingredients = [];
+
+  for (let ingredientsName in props.ingredients) {
+    ingredients.push({
+      name: ingredientsName,
+      amount: props.ingredients[ingredientsName],
+    });
+  }
+
+  const ingredientsOutput = ingredients.map((ig) => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border:'1px solid #ccc',
+          padding:'5px'
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount}){" "}
+      </span>
+    );
+  });
+
+  return (
+
     <div className={classes.Order}>
-        <p>Ingredients: Meat (1)</p>
-        <p>TotalPrice : <strong>USD 4.53</strong> </p>
+      <p>Ingredients: {ingredientsOutput} </p>
+      <p>
+        TotalPrice :
+        <strong>USD {Number.parseFloat(props.price).toFixed(2)} </strong>
+      </p>
     </div>
-);
+  );
+};
 
 export default order;
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,39 +1,49 @@
-import React, {Component} from 'react';
-import axios from '../../axios-orders';
+import React, { Component } from "react";
+import axios from "../../axios-orders";
 
-import Order from '../../components/Order/Order';
-import Spinner from '../../components/UI/Spinner/Spinner';
+import Order from "../../components/Order/Order";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
-class Orders extends Component{
-    state={
-        orders:[],
-        loading:true
-    }
-    componentDidMount(){
-        
-        axios.get('/orders.json')
-        .then(res=>{
-            const fetchedOrder=[];
-            for(key in res.data){
-                fetchedOrder.push({
-                    ...res.data[key],
-                    id:key
-                })
-            }
-            this.setState({ loading: false });
-        })
-        .catch(error=>{
-            this.setState({ loading: false });
-        });
-    }
-    render(){
-        return (
-          <div>
-            <Order />
-            <Order />
-          </div>
-        );
-    }
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrder = [];
+        for (let key in res.data) {
+          fetchedOrder.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrder });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+      });
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        {this.state.loading?<Spinner/>:null}  
+        {this.state.orders.map((order) => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
